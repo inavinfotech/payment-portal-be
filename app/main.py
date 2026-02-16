d99568d12e3308b45b0eaf91b6cb15b3acd41d1d
DeveloperSwastik <developerswastiksharma@gmail.com>
2026-02-16 11:40:36 +0530

    auto reload on

--- app/main.py
+++ app/main.py
@@ -43,3 +43,7 @@ scheduler.start()
 @app.on_event("shutdown")
 def shutdown_event():
     scheduler.shutdown()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
